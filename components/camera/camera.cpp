43e75bcb7860a4b226423b896fe62545edbeb14a
Cominta <romanitstudents@gmail.com>
2022-05-11 22:04:33 +0300

    небольшой рефакторинг камеры (мне кажется, резвость больше подойдет сюда)

--- components/camera/camera.cpp
+++ components/camera/camera.cpp
@@ -5,7 +5,7 @@ Camera::Camera(sf::RenderWindow* window, std::map<std::string, int>* bindKeys)
 {
     this->currentSpeedX = 0;
     this->currentSpeedY = 0;
-    this->speed = 2.0f;
+    this->speed = 300.0f;
 }
 
 Camera::~Camera()
@@ -18,63 +18,65 @@ void Camera::update(float delta)
     sf::View view {this->window->getView()};
 
     bool move = false;
+    this->currentSpeedX = 0;
+    this->currentSpeedY = 0;
 
     if (sf::Keyboard::isKeyPressed((sf::Keyboard::Key)this->bindKeys->at("camera-move-left")))
     {
-        this->currentSpeedX -= this->speed * delta;
+        this->currentSpeedX = -this->speed * delta;
         move = true;
     }
 
     if (sf::Keyboard::isKeyPressed((sf::Keyboard::Key)this->bindKeys->at("camera-move-right")))
     {
-        this->currentSpeedX += this->speed * delta;
+        this->currentSpeedX = this->speed * delta;
         move = true;
     }
 
     if (sf::Keyboard::isKeyPressed((sf::Keyboard::Key)this->bindKeys->at("camera-move-bottom")))
     {
-        this->currentSpeedY += this->speed * delta;
+        this->currentSpeedY = this->speed * delta;
         move = true;
     }
 
     if (sf::Keyboard::isKeyPressed((sf::Keyboard::Key)this->bindKeys->at("camera-move-top")))
     {
-        this->currentSpeedY -= this->speed * delta;
+        this->currentSpeedY = -this->speed * delta;
         move = true;
     }
 
-    else if (!move)
-    {
-        if (this->currentSpeedX < -3)
-        {
-            this->currentSpeedX += this->speed * delta;
-        }
+    // else if (!move)
+    // {
+    //     if (this->currentSpeedX < -3)
+    //     {
+    //         this->currentSpeedX += this->speed * delta;
+    //     }
 
-        else if (this->currentSpeedX > 3)
-        {
-            this->currentSpeedX -= this->speed * delta;
-        }
+    //     else if (this->currentSpeedX > 3)
+    //     {
+    //         this->currentSpeedX -= this->speed * delta;
+    //     }
 
-        else 
-        {
-            this->currentSpeedX = 0;
-        }
+    //     else 
+    //     {
+    //         this->currentSpeedX = 0;
+    //     }
 
-        if (this->currentSpeedY < -3)
-        {
-            this->currentSpeedY += this->speed * delta;
-        }
+    //     if (this->currentSpeedY < -1)
+    //     {
+    //         this->currentSpeedY += this->speed * delta;
+    //     }
 
-        else if (this->currentSpeedY > 3)
-        {
-            this->currentSpeedY -= this->speed * delta;
-        }
+    //     else if (this->currentSpeedY > 1)
+    //     {
+    //         this->currentSpeedY -= this->speed * delta;
+    //     }
 
-        else 
-        {
-            this->currentSpeedY = 0;
-        }
-    }
+    //     else 
+    //     {
+    //         this->currentSpeedY = 0;
+    //     }
+    // }
 
     view.move(this->currentSpeedX, this->currentSpeedY);
     this->window->setView(view);
